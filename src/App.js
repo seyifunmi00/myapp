2b7fd1dd5ca5a7ce0dae48f353412657f02cb218
seyitan001 <99769210+seyitan001@users.noreply.github.com>
2024-09-27 21:31:25 +0100

    real learing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-
+import Add from "./components/add";
+import  { useEffect, useState } from'react';
 
 function App() {
-  return (
-    <div className="App">
-   <h1>Hello</h1>
-    </div>
-  );
+
+  const [advice, setAdvice]= useState("");
+  const [number, setNumber] = useState(0);
+
+    async function getAdvice() {
+     const res = await fetch("https://api.adviceslip.com/advice");
+     const data =  await res.json();
+     setAdvice(data.slip.advice);
+     setNumber((number) + 1);
+    }
+   
+    useEffect(function () {
+      setAdvice();
+    }, []);
+    return (
+      <div>
+        <h1>{advice}</h1>
+        <button onClick={getAdvice}>Get Advice</button>
+        <p>You have read <strong>{number}</strong> advices</p>
+      </div>
+    );
+  
+  
 }
 
 export default App;
---
+++ src/components/add.js
@@ -0,0 +1,9 @@
+
+
+const add = () => {
+  return (
+    <div>I am from add</div>
+  )
+}
+
+export default add
